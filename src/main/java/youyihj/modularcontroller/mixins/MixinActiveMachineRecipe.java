f32a8435cd220b0b016a283127708ccc76d1b5d3
youyihj <youyi580@qq.com>
2021-11-27 17:04:00 +0800

    copy modifiers

--- src/main/java/youyihj/modularcontroller/mixins/MixinActiveMachineRecipe.java
+++ src/main/java/youyihj/modularcontroller/mixins/MixinActiveMachineRecipe.java
@@ -3,6 +3,8 @@ package youyihj.modularcontroller.mixins;
 import hellfirepvp.modularmachinery.common.crafting.ActiveMachineRecipe;
 import hellfirepvp.modularmachinery.common.crafting.MachineRecipe;
 import hellfirepvp.modularmachinery.common.crafting.helper.RecipeCraftingContext;
+import hellfirepvp.modularmachinery.common.crafting.requirement.type.RequirementType;
+import hellfirepvp.modularmachinery.common.modifier.RecipeModifier;
 import hellfirepvp.modularmachinery.common.tiles.TileMachineController;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
@@ -13,6 +15,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import youyihj.modularcontroller.event.MachineRecipeEventFactory;
+import youyihj.modularcontroller.util.IRecipeCraftingContextPatch;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author youyihj
@@ -25,6 +31,8 @@ public abstract class MixinActiveMachineRecipe {
 
     @Shadow
     private int tick;
+
+    private Map<RequirementType<?, ?>, List<RecipeModifier>> currentModifiers;
 
     @Inject(method = "complete", at = @At("RETURN"))
     private void postCompleteEvent(RecipeCraftingContext completionContext, CallbackInfo ci) {
@@ -49,5 +57,15 @@ public abstract class MixinActiveMachineRecipe {
             tick = 0;
             cir.setReturnValue(TileMachineController.CraftingStatus.failure(s));
         }
+    }
+
+    @Inject(method = "start", at = @At("RETURN"))
+    private void setCurrentModifiersOnStarting(RecipeCraftingContext context, CallbackInfo ci) {
+        currentModifiers = ((IRecipeCraftingContextPatch) context).getAllModifiers();
+    }
+
+    @Inject(method = "complete", at = @At("HEAD"))
+    private void putCurrentModifiersOnCompleted(RecipeCraftingContext completionContext, CallbackInfo ci) {
+        ((IRecipeCraftingContextPatch) completionContext).setModifiers(currentModifiers);
     }
 }
--- src/main/java/youyihj/modularcontroller/mixins/MixinRecipeCraftContext.java
+++ src/main/java/youyihj/modularcontroller/mixins/MixinRecipeCraftContext.java
@@ -1,15 +1,25 @@
 package youyihj.modularcontroller.mixins;
 
 import hellfirepvp.modularmachinery.common.crafting.helper.RecipeCraftingContext;
+import hellfirepvp.modularmachinery.common.crafting.requirement.type.RequirementType;
+import hellfirepvp.modularmachinery.common.modifier.RecipeModifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import youyihj.modularcontroller.event.MachineRecipeEventFactory;
+import youyihj.modularcontroller.util.IRecipeCraftingContextPatch;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author youyihj
  */
 @Mixin(value = RecipeCraftingContext.class, remap = false)
-public class MixinRecipeCraftContext {
+public abstract class MixinRecipeCraftContext implements IRecipeCraftingContextPatch {
+    @Shadow
+    private Map<RequirementType<?, ?>, List<RecipeModifier>> modifiers;
+
     /**
      * @author youyihj
      * @reason ?
@@ -17,5 +27,15 @@ public class MixinRecipeCraftContext {
     @Overwrite
     public RecipeCraftingContext.CraftingCheckResult canStartCrafting() {
         return MachineRecipeEventFactory.onStarted((RecipeCraftingContext) (Object) this);
+    }
+
+    @Override
+    public Map<RequirementType<?, ?>, List<RecipeModifier>> getAllModifiers() {
+        return modifiers;
+    }
+
+    @Override
+    public void setModifiers(Map<RequirementType<?, ?>, List<RecipeModifier>> modifiers) {
+        this.modifiers = modifiers;
     }
 }
