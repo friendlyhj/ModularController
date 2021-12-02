34d613e5cef7f3d3e78b01cb56246586b0e8e5e4
youyihj <youyi580@qq.com>
2021-12-02 16:01:01 +0800

    tooltip and texture

--- src/main/java/youyihj/modularcontroller/item/MachineProjector.java
+++ src/main/java/youyihj/modularcontroller/item/MachineProjector.java
@@ -2,9 +2,12 @@ package youyihj.modularcontroller.item;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -13,6 +16,9 @@ import net.minecraft.world.World;
 import youyihj.modularcontroller.ModularController;
 import youyihj.modularcontroller.block.BlockMMController;
 import youyihj.modularcontroller.core.Reference;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * @author youyihj
@@ -37,5 +43,10 @@ public class MachineProjector extends Item {
             return EnumActionResult.SUCCESS;
         }
         return EnumActionResult.PASS;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(I18n.format("tooltip.modularcontroller.machine_projector"));
     }
 }
