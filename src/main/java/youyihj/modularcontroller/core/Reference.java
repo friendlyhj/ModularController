cd39bb591393a7e1d7b37ea1fd8983ab97bb3164
youyihj <youyi580@qq.com>
2021-12-17 22:53:22 +0800

    1.2.2

--- src/main/java/youyihj/modularcontroller/core/Reference.java
+++ src/main/java/youyihj/modularcontroller/core/Reference.java
@@ -6,6 +6,6 @@ package youyihj.modularcontroller.core;
 public class Reference {
     public static final String MOD_ID = "modularcontroller";
     public static final String MOD_NAME = "Modular Controller";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
     public static final String DEPENDENCIES = "required-after:modularmachinery@[1.11.0,1.11.1];required-after:crafttweaker@[4.1.20,);required-after:mixinbooter";
 }
